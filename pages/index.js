9e5fcced6c404a20e5e3ac16e9e14ad21b5a94f9
Kazim Kayhan <kazimmohammadi400@gmail.com>
2022-07-12 11:49:56 +0430

    Pre render the home page data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from '../helpers/api-utils';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage({events}) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={events} />
     </div>
   );
+}
+
+export const getStaticProps = async () => {
+  const featuredEvents = await getFeaturedEvents();
+    return {
+      props:{
+        events:featuredEvents,
+      },
+      revalidate:90
+    }
 }
 
 export default HomePage;
